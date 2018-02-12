f6fad5afb99d8947a04a967ce4aa255437f4f124
Jon Gadsden <jon.gadsden@forgerock.com>
2018-02-12 15:33:21 +0000

    fixed up memory map

--- core/arch/arm/plat-artik/platform_config.h
+++ core/arch/arm/plat-artik/platform_config.h
@@ -59,39 +59,39 @@
 /*  LPUART 2 */
 #define UART3_BASE			0x02960000
 
-
 /* console uart define */
-#define CONSOLE_UART_BASE		UART0_BASE
-#define CONSOLE_BAUDRATE	115200
-#define CONSOLE_UART_CLK_IN_HZ	200000000
-
-#define DRAM0_BASE			0x80000000
+#define CONSOLE_UART_BASE        UART0_BASE
+#define CONSOLE_BAUDRATE         115200
+#define CONSOLE_UART_CLK_IN_HZ   200000000
 
 /* Platform specific defines */
-#define DRAM0_SIZE			0x80000000
-#define CFG_DDR_TEETZ_RESERVED_START	0xFC000000
-#define CFG_DDR_TEETZ_RESERVED_SIZE	0x03F00000
-#define CFG_TEE_RAM_VA_SIZE		(1024 * 1024)
-#define CFG_PUB_RAM_SIZE		(1024 * 1024)
-#define CFG_TEE_CORE_NB_CORE		2
+#define CFG_DDR_TEETZ_RESERVED_START  0xA8000000
+/* memory 63 + 1 MB */
+#define CFG_DDR_TEETZ_RESERVED_SIZE   0x03F00000
+#define CFG_TEE_RAM_VA_SIZE          (1024 * 1024)
+#define CFG_PUB_RAM_SIZE             (1024 * 1024)
+#define CFG_TEE_CORE_NB_CORE          2
 
-#define DDR_PHYS_START			DRAM0_BASE
-#define DDR_SIZE			DRAM0_SIZE
+/* ARTIK has 512MB DRAM */
+#define DRAM0_BASE           0x91000000
+#define DRAM0_SIZE           0x1F000000
+#define DDR_PHYS_START       DRAM0_BASE
+#define DDR_SIZE             DRAM0_SIZE
 
-#define CFG_DDR_START			DDR_PHYS_START
-#define CFG_DDR_SIZE			DDR_SIZE
+#define CFG_DDR_START        DDR_PHYS_START
+#define CFG_DDR_SIZE         DDR_SIZE
 
 #ifndef CFG_DDR_TEETZ_RESERVED_START
 #error "TEETZ reserved DDR start address undef: CFG_DDR_TEETZ_RESERVED_START"
 #endif
 #ifndef CFG_DDR_TEETZ_RESERVED_SIZE
-#error "TEETZ reserved DDR siez undefined: CFG_DDR_TEETZ_RESERVED_SIZE"
+#error "TEETZ reserved DDR size undefined: CFG_DDR_TEETZ_RESERVED_SIZE"
 #endif
 
 /*
  * TEE/TZ RAM layout:
  *
- *  +-----------------------------------------+  <- DRAM0_BASE + DRAM0_SIZE
+ *  +-----------------------------------------+  <- DRAM0_BASE
  *  |  Linux memory                           |
  *  +-----------------------------------------+  <- CFG_DDR_TEETZ_RESERVED_START
  *  | TEETZ private RAM  |  TEE_RAM  1MiB     |   ^
@@ -102,8 +102,8 @@
  *  |  TEE/TZ and NSec   |  PUB_RAM  1MiB ----|   |
  *  |   shared memory    |         NSec alloc |   |
  *  +-----------------------------------------+   v
- *  |  Linux memory                           |
- *  +-----------------------------------------+  <- DRAM0_BASE
+ *  |  Linux memory (Factory info)            |
+ *  +-----------------------------------------+  <- DRAM0_BASE + DRAM0_SIZE
  *  
  *  TEE_RAM : 1MByte
  *  PUB_RAM : 1MByte
