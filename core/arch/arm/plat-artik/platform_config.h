ebbaa4bd365121c4917c755a3b0af96cb6e32ebb
Jon Gadsden <jon.gadsden@forgerock.com>
2018-02-13 13:00:36 +0000

    fix-ups for UART reg address

--- core/arch/arm/plat-artik/platform_config.h
+++ core/arch/arm/plat-artik/platform_config.h
@@ -1,35 +1,21 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright (C) 2015 Freescale Semiconductor, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2018, ForgeRock Limited
  */
 
 #ifndef PLATFORM_CONFIG_H
 #define PLATFORM_CONFIG_H
 
 #define STACK_ALIGNMENT			64
+
+#define GIC_BASE      0x10220000
+#define GICC_OFFSET   0x2000
+#define GICD_OFFSET   0x1000
+
+#define UART0_BASE    0x11002000
+#define UART1_BASE    0x11003000
+#define UART2_BASE    0x11004000
+#define UART3_BASE    0x11005000
 
 /* console uart define */
 #define CONSOLE_UART_BASE        UART0_BASE
@@ -68,18 +54,19 @@
  *  +-----------------------------------------+  <- CFG_DDR_TEETZ_RESERVED_START
  *  | TEETZ private RAM  |  TEE_RAM  1MiB     |   ^
  *  |                    +--------------------+   |
- *  |                    |  TA_RAM            |   |
+ *  |                    |  TA_RAM   61MiB    |   |
  *  +-----------------------------------------+   | CFG_DDR_TEETZ_RESERVED_SIZE
  *  |                    |      teecore alloc |   |
  *  |  TEE/TZ and NSec   |  PUB_RAM  1MiB ----|   |
  *  |   shared memory    |         NSec alloc |   |
  *  +-----------------------------------------+   v
- *  |  Linux memory (Factory info)            |
+ *  |  Linux memory (Factory info 64MiB)      |
  *  +-----------------------------------------+  <- DRAM0_BASE + DRAM0_SIZE
  *  
  *  TEE_RAM : 1MByte
  *  PUB_RAM : 1MByte
- *  TA_RAM  : all what is left (at least 2MByte !)
+ *  TA_RAM  : 61MByte
+ *  Total   : 63MiB
  */
 
 /* define the several memory area sizes */
